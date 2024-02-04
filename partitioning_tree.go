89e879b14f40418ee6b5e4ffb85abd7f822d7ade
Roelof Ruis <roelof.ruis@gmail.com>
2024-02-04 01:20:20 +0100

    adding the child nodes

--- partitioning_tree.go
+++ partitioning_tree.go
@@ -12,6 +12,8 @@ func (i Interval) String() string {
 
 type PartitioningNode struct {
 	Interval Interval
+	Left     *PartitioningNode
+	Right    *PartitioningNode
 }
 
 type PartitioningTree struct {
@@ -22,15 +24,37 @@ func NewPartitioningTree() *PartitioningTree {
 	return &PartitioningTree{}
 }
 
-func (t *PartitioningTree) Insert(i Interval) {
+func (t *PartitioningTree) Insert(i Interval) bool {
 	newNode := &PartitioningNode{Interval: i}
 
 	if t.Root == nil {
 		t.Root = newNode
-		return
+		return true
 	}
 
-	// test if fits
+	currentNode := t.Root
+	for {
+		if i.Start >= currentNode.Interval.End {
+			// It can be placed after
+			if currentNode.Right == nil {
+				currentNode.Right = newNode
+				return true
+			}
+			currentNode = currentNode.Right
+			continue
+		}
+		if i.End <= currentNode.Interval.Start {
+			// It can be placed before
+			if currentNode.Left == nil {
+				currentNode.Left = newNode
+				return true
+			}
+			currentNode = currentNode.Left
+			continue
+		}
+
+		return false
+	}
 }
 
 func (t *PartitioningTree) TraverseInOrder(f func(i Interval)) {
@@ -39,7 +63,9 @@ func (t *PartitioningTree) TraverseInOrder(f func(i Interval)) {
 		if node == nil {
 			return
 		}
+		inOrder(node.Left)
 		f(node.Interval)
+		inOrder(node.Right)
 	}
 
 	inOrder(t.Root)
